4f20d699df4400e1757b8b6f52ac8da7303838b5
Pete Skeggs <peter.skeggs@nordicsemi.no>
2020-03-06 15:34:17 -0800

    - get cloud client sample to build again

--- samples/nrf9160/cloud_client/src/main.c
+++ samples/nrf9160/cloud_client/src/main.c
@@ -155,7 +155,7 @@ void main(void)
 	}
 #endif
 
-	err = cloud_connect(cloud_backend, NULL, NULL);
+	err = cloud_connect(cloud_backend, NULL);
 	if (err) {
 		printk("cloud_connect, error: %d\n", err);
 	}
