7481d70d0f4614d02b67adbd04b1342bca713059
Pete Skeggs <peter.skeggs@nordicsemi.no>
2020-05-20 10:31:32 -0700

    add NRF_SO_SNDTIMEO

--- lib/bsdlib/nrf91_sockets.c
+++ lib/bsdlib/nrf91_sockets.c
@@ -151,6 +151,9 @@ static int z_to_nrf_optname(int z_in_level, int z_in_optname,
 		case SO_RCVTIMEO:
 			*nrf_out_optname = NRF_SO_RCVTIMEO;
 			break;
+		case SO_SNDTIMEO:
+			*nrf_out_optname = NRF_SO_SNDTIMEO;
+			break;
 		case SO_BINDTODEVICE:
 			*nrf_out_optname = NRF_SO_BINDTODEVICE;
 			break;
