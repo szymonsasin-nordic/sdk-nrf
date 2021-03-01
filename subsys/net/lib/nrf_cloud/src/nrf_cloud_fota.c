3cdc393d28b10a3f442fb84c69b907acdf5b4bcc
Pete Skeggs <peter.skeggs@nordicsemi.no>
2021-03-01 13:20:43 -0800

    - check in fota fix (rebase later once Justin's PR is merged with NCS)

--- subsys/net/lib/nrf_cloud/src/nrf_cloud_fota.c
+++ subsys/net/lib/nrf_cloud/src/nrf_cloud_fota.c
@@ -22,6 +22,7 @@
 #include <settings/settings.h>
 #include <power/reboot.h>
 #include <cJSON.h>
+#include <cJSON_os.h>
 
 #if defined(CONFIG_BOOTLOADER_MCUBOOT)
 #include <dfu/mcuboot.h>
@@ -720,7 +721,7 @@ static int parse_job_info(struct nrf_cloud_fota_job_info *const job_info,
 	temp = cJSON_PrintUnformatted(array);
 	if (temp) {
 		LOG_DBG("JSON array: %s", log_strdup(temp));
-		cJSON_free(temp);
+		cJSON_FreeString(temp);
 	}
 
 #if CONFIG_NRF_CLOUD_FOTA_BLE_DEVICES
@@ -785,7 +786,7 @@ cleanup:
 	memset(job_info, 0, sizeof(*job_info));
 	job_info->type = NRF_CLOUD_FOTA_TYPE__INVALID;
 	if (array) {
-		cJSON_free(array);
+		cJSON_Delete(array);
 	}
 	return err;
 }
@@ -842,6 +843,7 @@ static int start_job(struct nrf_cloud_fota_job *const job)
 	return ret;
 }
 
+extern uint32_t heap_stats(bool print);
 static void cleanup_job(struct nrf_cloud_fota_job *const job)
 {
 	__ASSERT_NO_MSG(job != NULL);
@@ -849,10 +851,11 @@ static void cleanup_job(struct nrf_cloud_fota_job *const job)
 		job->info.id ? log_strdup(job->info.id) : "N/A");
 
 	if (job->parsed_payload) {
-		cJSON_free(job->parsed_payload);
+		cJSON_Delete(job->parsed_payload);
 	}
 	memset(job, 0, sizeof(*job));
 	job->info.type = NRF_CLOUD_FOTA_TYPE__INVALID;
+	heap_stats(true);
 }
 
 static int publish(const struct mqtt_publish_param *const pub)
@@ -884,7 +887,7 @@ static int publish_and_free_array(cJSON *const array,
 	int ret;
 	char *array_str = cJSON_PrintUnformatted(array);
 
-	cJSON_free(array);
+	cJSON_Delete(array);
 
 	if (array_str == NULL) {
 		return -ENOMEM;
@@ -899,7 +902,7 @@ static int publish_and_free_array(cJSON *const array,
 		ret = -EINVAL;
 	}
 
-	cJSON_free(array_str);
+	cJSON_FreeString(array_str);
 
 	return ret;
 }
@@ -970,7 +973,7 @@ static int send_job_update(struct nrf_cloud_fota_job *const job)
 	}
 
 	if (!result) {
-		cJSON_free(array);
+		cJSON_Delete(array);
 		return -ENOMEM;
 	}
 
@@ -1084,7 +1087,7 @@ send_ack:
 
 	if (skip || job_info->type == NRF_CLOUD_FOTA_TYPE__INVALID) {
 		if (payload_array) {
-			cJSON_free(payload_array);
+			cJSON_Delete(payload_array);
 		}
 		return ret;
 	}
@@ -1094,7 +1097,7 @@ send_ack:
 		if (ble_cb) {
 			ble_cb(&ble_job);
 		}
-		cJSON_free(payload_array);
+		cJSON_Delete(payload_array);
 #endif
 	} else {
 		/* Save JSON to current fota and start update */
@@ -1236,7 +1239,7 @@ int nrf_cloud_fota_ble_update_check(const bt_addr_t *const ble_id)
 	}
 
 	if (!add_string_to_array(array, ble_id_str)) {
-		cJSON_free(array);
+		cJSON_Delete(array);
 		return -ENOMEM;
 	}
 
@@ -1287,7 +1290,7 @@ int nrf_cloud_fota_ble_job_update(const struct nrf_cloud_fota_ble_job
 	}
 
 	if (!result) {
-		cJSON_free(array);
+		cJSON_Delete(array);
 		return -ENOMEM;
 	}
 
